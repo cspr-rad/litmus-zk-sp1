f16df45a917d86538b5864a6fbbda5124f0a0b5c
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-11-08 11:00:30 +0100

    1. Extending typeset with primitives.

--- crates/lcodecs/src/binary/constants.rs
+++ crates/lcodecs/src/binary/constants.rs
@@ -22,6 +22,15 @@ pub(super) const ENCODED_SIZE_U32: usize = core::mem::size_of::<u32>();
 /// Encoded size: `u64`.
 pub(super) const ENCODED_SIZE_U64: usize = core::mem::size_of::<u64>();
 
+/// The number of bytes in a serialized [`U128`](crate::U128).
+pub(super) const ENCODED_SIZE_U128: usize = core::mem::size_of::<u128>();
+
+/// The number of bytes in a serialized [`U256`](crate::U256).
+pub(super) const ENCODED_SIZE_U256: usize = ENCODED_SIZE_U128 * 2;
+
+/// The number of bytes in a serialized [`U512`](crate::U512).
+pub(super) const ENCODED_SIZE_U512: usize = ENCODED_SIZE_U256 * 2;
+
 /// The tag representing a `None` value.
 pub(super) const TAG_OPTION_NONE: u8 = 0;
 
