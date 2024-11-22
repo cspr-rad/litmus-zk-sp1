d24085cd26da51533e54d99f44b0df0b395130d7
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-11-22 17:49:02 +0100

    1. Minor tweaks.

--- crates/lverifiers/src/lib.rs
+++ crates/lverifiers/src/lib.rs
@@ -9,15 +9,10 @@ pub fn verify_block_v1_with_proofs(_: BlockWithProofs) {
 pub fn verify_block_v2_with_proofs(entity: BlockWithProofs, chain_name_digest: ChainNameDigest) {
     // 1. Validate finality signatures.
     let msg = match entity.block() {
-        Block::V1(_) => panic!(
-            "Block type version to verifier mismatch.  Invoke verify_block_v1_with_proofs instead."
-        ),
         Block::V2(inner) => inner.get_bytes_for_finality_signature(&chain_name_digest),
+        _ => panic!("Invalid block version."),
     };
     for proof in entity.proofs() {
         proof.signature().verify(proof.verification_key(), &msg);
     }
-
-    // 1. Recompute block hash & assert equivalence.
-    // 2. Verify signatures computed by validator public keys within era scope.
 }
